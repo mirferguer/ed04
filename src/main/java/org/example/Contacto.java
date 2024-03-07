caa04049798419694b47e91ab9e52af784b661a1
ed04 <13162512@alu.murciaeduca.es>
2024-03-07 09:56:48 +0100

    Encapsular los dos atributos de la clase Contacto.

--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -35,4 +35,12 @@ class Contacto {
     public List<String> getPhones() {
         return this.phones;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
+    }
 }
